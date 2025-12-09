f9c10f7e43e12bad8423464c60ce6e864ccd7fb4
marwa youssef <marwayoussef678@gmail.com>
2025-12-09 15:20:30 +0200

    setup feartures[sendmail]& create [book-user]model

--- models/Book.js
+++ models/Book.js
@@ -1,7 +1,25 @@
+const { ref } = require("joi");
 const mongoose = require("mongoose");
 
 const BookSchema = new mongoose.Schema({
   // add fields here
-});
+title:{type:String,required:true},
 
-module.exports = mongoose.model("Book", BookSchema);
+author:{type:String},
+
+category:{type:String,required:true},
+//url
+coverImage:{type:String},
+
+stock:{type:String,required:true,
+  default:1},
+
+  price:{type:Number,required:true},
+
+  //relation  (User VS Book)  1-M
+userId:{type:mongoose.SchemaTypes.ObjectId,ref:"User",required:true},
+
+},{timestamps:true});
+const Book = mongoose.model("Book",BookSchema);
+
+module.exports = {Book}
--- models/User.js
+++ models/User.js
@@ -2,6 +2,30 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   // add fields here
-});
+name:{type:String,required:true,trim:true},
 
-module.exports = mongoose.model("User", UserSchema);
+email:{type:String,required:true,unique:true},
+
+password:{type:String,required:true},
+
+role:{type:String,enum:["user","admin"],default:"user"},
+
+ otp:{type:String,maxLength:6},
+
+  otpExpires:{type:Date},
+
+  isVerify:{type:Boolean,default:false},
+
+  resetPasswordToken:{type:String},
+  
+  resetPasswordExpiresIN:{type:Date},
+
+},{timestamps:true});
+
+
+
+
+const User=mongoose.model("User",UserSchema)
+
+
+module.exports ={User} ;
