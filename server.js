43e302b589e12307ade6c763b84a5aaec8a599e9
marwa youssef <marwayoussef678@gmail.com>
2025-12-09 13:29:15 +0200

    setup database connection

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
-require("dotenv").config();
+
+
+//Imports
 const express = require("express");
-const connectDB = require("./utils/db");
+const dotenv = require("dotenv");
+//config
+dotenv.config();
+//
 const authRoutes = require("./routes/auth");
 const bookRoutes = require("./routes/books");
 const purchaseRoutes = require("./routes/purchases");
 const errorHandler = require("./middleware/errorHandler");
+const{connectToDatabase}=require("./utils/db");
 
+
+//APP
 const app = express();
+
+//Global
 app.use(express.json());
-
-connectDB();
-
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
 app.use("/api/purchases", purchaseRoutes);
 
 app.use(errorHandler);
+//connection Mongo DB
+connectToDatabase();
+//Main Routes
+app.get("/",(request,response)=>{
+  response.send("Welcome To Backend .")
+})
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,22 @@
-const connectDB = async () => {
+
+
+//Imports 
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+//Config
+dotenv.config();
+
+//connection DB
+async function connectToDatabase(){
   // build connection here
+try {
+  await mongoose.connect(process.env.CONNECTION_STRING);
+  console.log(`Mongo Cloud Connection Successfully`)
+} catch (error) {
+  console.log(error)
+}
+
 };
 
-module.exports = connectDB;
+module.exports ={connectToDatabase}
